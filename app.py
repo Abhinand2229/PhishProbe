f15596e46670bc25e30fde9608866db1ac495957
V Abhinand <2229@tkmce.ac.in>
2024-04-14 15:44:27 +0530

    File Added

---
+++ app.py
@@ -0,0 +1,52 @@
+from flask import Flask, request, render_template
+from controller import Controller
+import static.onetimescript as onetimescript
+from instance.db import db
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///domains.db'
+db.init_app(app)
+with app.app_context():
+    db.create_all() 
+
+controller = Controller()
+
+
+@app.route('/',  methods=['GET','POST'])
+def home():
+    
+    try:
+        url = request.form['url']
+        result = controller.main(url)
+        output = result
+    except:
+        output = 'NA'
+
+    return render_template('index.html', output=output)
+
+@app.route('/update-db')
+def update_db(): 
+    try:
+        with app.app_context():
+            response = onetimescript.update_db()
+            print("Database populated successfully!")
+            return response, 200
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+        return "An error occurred: " + str(e), 500
+
+@app.route('/update-json')
+def update_json(): 
+    try:
+        with app.app_context():
+            response = onetimescript.update_json()
+            print("JSON updated successfully!")
+            return response, 200
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+        return "An error occurred: " + str(e), 500
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
